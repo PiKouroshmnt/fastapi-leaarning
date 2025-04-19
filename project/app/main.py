e8fee913a4666a718b95d6d6bccaa1d0753e2533
kourosh montazeri <pico.mnt83@gmail.com>
2025-04-19 13:24:16 +0330

    clean up main and added db initialization and schema generation to db.py

--- project/app/main.py
+++ project/app/main.py
@@ -1,23 +1,24 @@
-import os
+import logging
 
-from fastapi import FastAPI, Depends
-from tortoise.contrib.fastapi import register_tortoise
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 
 from app.api import ping
+from app.db import init_db
+
+log = logging.getLogger("uvicorn")
 
 def create_application() -> FastAPI:
-    application = FastAPI()
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=False,
-        add_exception_handlers=True,
-    )
-
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        log.info("Starting up...")
+        init_db(app)
+        yield
+        log.info("Shutting down...")
+    application = FastAPI(lifespan=lifespan)
     application.include_router(ping.router)
 
     return application
 
 app = create_application()
+
